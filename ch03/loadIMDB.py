db1e4077f9d8d967106202f23ed620bfd494bf03
guohui13 <guohui13@163.com>
2021-01-07 01:38:44 +0800

    add at 2021.1.7

---
+++ ch03/loadIMDB.py
@@ -0,0 +1,14 @@
+from keras.datasets import imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+# print(train_data[0])
+# print(train_labels[0])
+
+print(max(max(sequence) for sequence in train_data))
+
+
+word_index = imdb.get_word_index()
+# print(word_index)
+# reverse_word_index = dict([(value,key) for (key,value) in word_index.items()])
+# decoded_review = ' '.join()
