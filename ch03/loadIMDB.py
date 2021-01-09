475b7f42db6c4ab93502ed2b515665183ade8915
guohui13 <guohui13@163.com>
2021-01-09 18:58:06 +0800

    add at 2021.1.7

--- ch03/loadIMDB.py
+++ ch03/loadIMDB.py
@@ -4,6 +4,7 @@ from keras import layers
 from keras import optimizers
 from keras import losses
 from keras import metrics
+import matplotlib.pyplot as plt
 import numpy as np
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
@@ -54,6 +55,45 @@ model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 
 history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
 
-history_dict=history.history
+history_dict = history.history
 
 print(dir(history_dict))
+print(history_dict.keys())
+
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+
+print('loss_values : ', loss_values)
+
+epochs = range(1, len(loss_values) + 1)
+
+plt.plot(epochs, loss_values, 'bo', label='Traing loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('loss')
+plt.legend()
+
+plt.show()
+
+plt.clf()
+acc = history_dict['acc']
+val_acc = history_dict['val_acc']
+plt.plot(epochs, acc, 'bo', label='Traing acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Traing and validationg accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('accuracy')
+plt.legend()
+plt.show()
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=3, batch_size=512)
+results=model.evaluate(x_test,y_test)
+print(results)
