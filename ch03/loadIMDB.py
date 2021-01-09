d07c3a6f55b4d1617f5a82737dea30ebfb2b4a56
guohui13 <guohui13@163.com>
2021-01-09 19:40:36 +0800

    add at 2021.1.9

--- ch03/loadIMDB.py
+++ ch03/loadIMDB.py
@@ -88,15 +88,16 @@ plt.legend()
 plt.show()
 
 model = models.Sequential()
-model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(32, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=3, batch_size=512)
-results=model.evaluate(x_test,y_test)
+results = model.evaluate(x_test, y_test)
 print(results)
 
-prediction=model.predict(x_test)
+prediction = model.predict(x_test)
 print(prediction)
