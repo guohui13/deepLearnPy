003aad9bfc77b938427527f401e47a847541122e
guohui13 <guohui13@163.com>
2021-01-11 01:08:46 +0800

    add at 2021.1.11

--- ch02/loadMinstfromKeras.py
+++ ch02/loadMinstfromKeras.py
@@ -20,10 +20,10 @@ train_images = train_images.astype('float32') / 255
 
 test_images = test_images.reshape((10000, 28 * 28))
 test_images = test_images.astype('float32') / 255
-
+print(train_labels[10])
 train_labels = to_categorical(train_labels)
-test_labels = to_categorical(test_labels)
-
+test_labels = to_categorical(test_labels) #one-hot编码
+print(train_labels[10])
 network.fit(train_images, train_labels, epochs=5, batch_size=128)
 
 test_loss, test_acc = network.evaluate(test_images, test_labels)
